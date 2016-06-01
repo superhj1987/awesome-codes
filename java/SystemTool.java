778ae69cdcd1b5ef29bd8458bba592f59d213036
Bryant Hang <superhj1987@126.com>
2016-06-01 22:09:16 +0800

    Create SystemTool.java

---
+++ java/SystemTool.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+public class SystemTool {
+    public void killByPid(String str) {
+        final String[] Array = {"ntsd.exe", "-c", "q", "-p", str};
+        int i = 0;
+        try {
+            Process process = Runtime.getRuntime().exec(Array);
+            process.waitFor();
+        } catch (InterruptedException e) {
+            System.out.println("run err!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (i != 0) {
+            try {
+                Process process = Runtime.getRuntime().exec(Array);
+                process.waitFor();
+            } catch (InterruptedException e) {
+                System.out.println("err!");
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
